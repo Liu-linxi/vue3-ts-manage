bf63c8a958c093fbe73c2badead8450f64887bc3
linxi <1595818766@qq.com>
2024-08-14 16:28:30 +0800

    标题模拟demo编写

---
+++ src/demo.vue
@@ -0,0 +1,24 @@
+<script lang="ts">
+import {defineComponent} from "vue"
+// export default {
+//   props: {},
+//   data() {return {} },
+//   methods: {},
+//   setup() {}
+// }
+export default defineComponent({
+  props:{}, 
+  components:{},
+  data(){return{}},
+  methods:{},
+  computed:{}
+})
+</script>
+
+<template>
+  <div class="home">
+
+  </div>
+</template>
+
+<style lang="less" scoped></style>
