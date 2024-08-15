a9035a8fda0f1ce0c1171c01d6fd0c624586d05f
linxi <1595818766@qq.com>
2024-08-15 10:50:35 +0800

    初始化项目

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,6 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
+import { createApp } from "vue";
 // 正常引入.vue文件为什么没有报错呢,正常情况需要已经声明过,通过查找env.d.ts已经声明过TypeScript Vue Plugin (Volar)默认起作用
 // 不确定是什么类型,相当于当成any类型,定义让其认为是一个组件来使用
-import App from './App.vue'
+import App from "./App.vue";
 
-import Demo from './demo.vue'
-
-
-
-createApp(App).mount('#app')
+createApp(App).mount("#app");
