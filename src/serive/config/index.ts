eefb69e7ba03fc750d18f43c9a5b634b04873dbc
linxi <1595818766@qq.com>
2024-09-09 11:40:30 +0800

    开发生产环境判断

--- src/serive/config/index.ts
+++ src/serive/config/index.ts
@@ -1,2 +1,9 @@
-export const BASE_URL1 = "http://codercba.com:8888"; //http://codercba.com:8888  http://152.136.185.210:5000
+// export const BASE_URL1 = "http://codercba.com:8888"; //http://codercba.com:8888  http://152.136.185.210:5000
 export const TIME_OUT1 = 10000;
+let BASE_URL1 = "";
+if (import.meta.env.MODE === "production") {
+  BASE_URL1 = "XXX";
+} else {
+  BASE_URL1 = "XXXXX";
+}
+export { BASE_URL1 };
