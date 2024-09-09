32a519dc691e6ad81f426d20fbd81b18b579cd08
linxi <1595818766@qq.com>
2024-09-09 17:48:40 +0800

    登陆页面编写

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,13 +1,21 @@
 <script setup lang="ts">
-
+import LoginPanel from './cpns/login-panel.vue'
 </script>
 
 <template>
   <div class="login">
-    登录
+    <login-panel />
   </div>
 </template>
 
 <style lang="less" scoped>
-.login {}
+.login {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: url("../../assets/img/login_bg.svg") no-repeat;
+  background-size: cover;
+}
 </style>
