fc990e00189765e116f8c0745b8829937167a0c9
linxi <1595818766@qq.com>
2024-09-13 17:48:45 +0800

    左侧导航布局

---
+++ src/components/main-menu/main-menu.vue
@@ -0,0 +1,111 @@
+<script setup>
+</script>
+
+<template>
+  <div class="main-menu">
+    <div class="logo">
+      <img class="img" src="@/assets/img/logo.svg" alt="logo" />
+      <span class="title">管理系统</span>
+    </div>
+    <div class="menu">
+      <el-menu class="el-menu-vertical-demo" text-color="#b7bdc3" active-text-color="#fff" background-color="#001529">
+        <el-sub-menu index="1">
+          <template #title>
+            <el-icon>
+              <Monitor />
+            </el-icon>
+            <span>系统总览</span>
+          </template>
+          <el-menu-item>核心技术</el-menu-item>
+          <el-menu-item>商品统计</el-menu-item>
+        </el-sub-menu>
+
+        <el-sub-menu index="2">
+          <template #title>
+            <el-icon>
+              <Setting />
+            </el-icon>
+            <span>系统管理</span>
+          </template>
+          <el-menu-item>用户管理</el-menu-item>
+          <el-menu-item>部门管理</el-menu-item>
+          <el-menu-item>菜单管理</el-menu-item>
+          <el-menu-item>角色管理</el-menu-item>
+        </el-sub-menu>
+
+
+
+        <el-sub-menu index="3">
+          <template #title>
+            <el-icon>
+              <Handbag />
+            </el-icon>
+            <span>商品中心</span>
+          </template>
+          <el-menu-item>商品类别</el-menu-item>
+          <el-menu-item>商品信息</el-menu-item>
+        </el-sub-menu>
+
+        <el-sub-menu index="4">
+          <template #title>
+            <el-icon>
+              <ChatLineRound />
+            </el-icon>
+            <span>随使聊聊</span>
+          </template>
+          <el-menu-item>你的故事</el-menu-item>
+          <el-menu-item>故事列表</el-menu-item>
+        </el-sub-menu>
+      </el-menu>
+    </div>
+  </div>
+</template>
+
+<style lang="less" scoped>
+.main-menu {
+  height: 100%;
+  background-color: #001529;
+
+  .logo {
+    display: flex;
+    height: 3rem;
+    padding: .75rem .625rem .5rem .625rem;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: center;
+    overflow: hidden;
+
+    .img {
+      height: 100%;
+      margin: 0 .625rem;
+    }
+
+    .title {
+      font-size: 1rem;
+      font-weight: 700;
+      color: white;
+      white-space: nowrap;
+    }
+  }
+}
+
+.el-menu {
+  border-right: none;
+  user-select: none;
+}
+
+.el-sub-menu {
+  .el-menu-item {
+    padding-left: 50px !important;
+    background-color: #0c2135;
+  }
+
+  .el-menu-item:hover {
+    color: #fff;
+  }
+
+  .el-menu-item.is-active {
+    background-color: #0a60bd;
+  }
+}
+</style>
