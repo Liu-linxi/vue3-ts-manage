99021a86815773ae918fc2cff6fda36aa9a74022
linxi <1595818766@qq.com>
2024-09-18 14:39:35 +0800

    面包屑导航显示

--- src/components/main-header/c-cpns/header-crumb.vue
+++ src/components/main-header/c-cpns/header-crumb.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="header-crumbs">
     <el-breadcrumb separator="/">
-      <!-- <template v-for="(item, index) in breadcrumbs" :key="index">
+      <template v-for="(item, index) in breadcrumbs" :key="index">
         <el-breadcrumb-item :to="item.path">{{ item.name }}</el-breadcrumb-item>
-      </template> -->
+      </template>
     </el-breadcrumb>
   </div>
 </template>
@@ -11,15 +11,15 @@
 <script setup lang="ts" name="header-crumbs">
 import { computed } from "vue";
 import { useRoute } from "vue-router";
-// import { mapPathToBreadcrumbs } from "@/utils/map-menu";
+import { mapPathToBreadcrumbs } from "@/utils/map-menu";
 import useLoginStore from "@/store/login/login";
 
-// const breadcrumbs = computed(() => {
-//   const route = useRoute();
-//   const loginStore = useLoginStore();
-//   const breadcrumbs = mapPathToBreadcrumbs(loginStore.userMenus, route.path);
-//   return breadcrumbs;
-// });
+const route = useRoute();
+const breadcrumbs = computed(() => {
+  const loginStore = useLoginStore();
+  const breadcrumbs = mapPathToBreadcrumbs(loginStore.userMenus, route.path);
+  return breadcrumbs;
+});
 </script>
 
 <style scoped lang="less"></style>
