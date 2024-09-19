f80dc00329a9febb5d1c36888beb62dcba865b25
linxi <1595818766@qq.com>
2024-09-19 14:05:33 +0800

    分装配置组件文件动态渲染

--- src/views/main/system/department/config/search.config.ts
+++ src/views/main/system/department/config/search.config.ts
@@ -1,26 +1,26 @@
 const searchConfig = {
-  pageName: 'department',
+  pageName: "department",
   formItems: [
     {
-      label: '部门名称',
-      prop: 'name',
-      type: 'input',
-      initialValue: 'aaa',
-      placeholder: '请输入部门名称'
+      label: "部门名称",
+      prop: "name",
+      type: "input",
+      initialValue: "",
+      placeholder: "请输入部门名称",
     },
     {
-      label: '部门领导',
-      prop: 'leader',
-      type: 'input',
-      placeholder: '请输入部门领导'
+      label: "部门领导",
+      prop: "leader",
+      type: "input",
+      placeholder: "请输入部门领导",
     },
     {
-      label: '创建时间',
-      prop: 'createAt',
-      type: 'date-picker',
-      placeholder: '请选择创建时间范围'
-    }
-  ]
-}
+      label: "创建时间",
+      prop: "createAt",
+      type: "date-picker",
+      placeholder: "请选择创建时间范围",
+    },
+  ],
+};
 
-export default searchConfig
+export default searchConfig;
