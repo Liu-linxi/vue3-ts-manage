22909f7e5a9eba8475505111c6da68758024af11
linxi <1595818766@qq.com>
2024-09-19 17:36:55 +0800

    角色界面新增

--- src/views/main/system/role/config/search.config.ts
+++ src/views/main/system/role/config/search.config.ts
@@ -1,24 +1,24 @@
 const searchConfig = {
-  pageName: 'role',
+  pageName: "role",
   formItems: [
     {
-      label: '角色名称',
-      prop: 'name',
-      type: 'input',
-      placeholder: '请输入角色名称'
+      label: "角色名称",
+      prop: "name",
+      type: "input",
+      placeholder: "请输入角色名称",
     },
     {
-      label: '角色权限',
-      prop: 'intro',
-      type: 'input',
-      placeholder: '请输入角色权限'
+      label: "角色权限",
+      prop: "intro",
+      type: "input",
+      placeholder: "请输入角色权限",
     },
     {
-      label: '创建时间',
-      prop: 'createAt',
-      type: 'date-picker'
-    }
-  ]
-}
+      label: "创建时间",
+      prop: "createAt",
+      type: "date-picker",
+    },
+  ],
+};
 
-export default searchConfig
+export default searchConfig;
