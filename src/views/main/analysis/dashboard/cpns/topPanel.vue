d28bb1fca38348527bad8ce0f6f0498c5c63b81c
linxi <1595818766@qq.com>
2024-09-23 09:17:52 +0800

    新增详情面板

---
+++ src/views/main/analysis/dashboard/cpns/topPanel.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts" name="topPanel">
+
+</script>
+
+<template>
+  <div class="topPanel">
+    <div class="panel" v-for=" li in 4" :key="li">
+      <div class="flex">
+        <div>
+          商品{{ li }}销量
+        </div>
+        <el-icon>
+          <Warning />
+        </el-icon>
+      </div>
+      <h2 class="num">{{ li }}</h2>
+      <div class="desc">
+        商品{{ li }}总销量{{ li }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="less" scoped>
+.topPanel {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 20px;
+
+  .panel {
+    padding: 10px 20px;
+    box-shadow: 0 0 12px 0px #ccc;
+
+    .flex {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      color: #666;
+      padding-bottom: 10px;
+    }
+
+    .num {
+      border-bottom: 1px solid #ccc;
+      padding-bottom: 10px;
+    }
+
+    .desc {
+      margin-top: 10px;
+    }
+  }
+}
+</style>
