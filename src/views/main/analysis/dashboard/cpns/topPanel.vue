2257ec937ad5058ce2cebaf80db04554f52310e8
linxi <1595818766@qq.com>
2024-09-29 14:46:49 +0800

    子组件修整规划

--- src/views/main/analysis/dashboard/cpns/topPanel.vue
+++ src/views/main/analysis/dashboard/cpns/topPanel.vue
@@ -1,52 +1,60 @@
 <script setup lang="ts" name="topPanel">
+import { CountUp } from 'countup.js';
+import { onMounted, ref } from 'vue';
 
+const numRef = ref<HTMLElement>()
+const props = withDefaults(defineProps<{
+  value?: number;
+}>(), {
+  value: 23423423,
+});
+
+// 参数1执行动画元素
+onMounted(() => {
+  const countUp = new CountUp(numRef.value!, props.value, {})
+  countUp.start()
+})
 </script>
 
 <template>
   <div class="topPanel">
-    <div class="panel" v-for=" li in 4" :key="li">
+    <div class="panel">
       <div class="flex">
         <div>
-          商品{{ li }}销量
+          商品XXX销量
         </div>
         <el-icon>
           <Warning />
         </el-icon>
       </div>
-      <h2 class="num">{{ li }}</h2>
+      <h2 class="num" ref="numRef">{{ value }}</h2>
       <div class="desc">
-        商品{{ li }}总销量{{ li }}
+        商品XXX总销量{{ value }}
       </div>
     </div>
   </div>
 </template>
 
 <style lang="less" scoped>
-.topPanel {
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  grid-gap: 20px;
+.panel {
+  padding: 10px 20px;
+  box-shadow: 0 0 12px 0px #ccc;
 
-  .panel {
-    padding: 10px 20px;
-    box-shadow: 0 0 12px 0px #ccc;
+  .flex {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    color: #666;
+    padding-bottom: 10px;
+  }
 
-    .flex {
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-      color: #666;
-      padding-bottom: 10px;
-    }
+  .num {
+    border-bottom: 1px solid #ccc;
+    padding-bottom: 10px;
+  }
 
-    .num {
-      border-bottom: 1px solid #ccc;
-      padding-bottom: 10px;
-    }
-
-    .desc {
-      margin-top: 10px;
-    }
+  .desc {
+    margin-top: 10px;
   }
 }
 </style>
