ad864cfee36f7aa25b4d3965e6fbd6025133d970
linxi <1595818766@qq.com>
2024-09-29 15:40:03 +0800

    展示示例

---
+++ src/views/main/analysis/dashboard/cpns/chartCard.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="chart-card">
+    <!-- <el-card>
+      <template #header>
+        <div class="card-header">
+          <span>卡片头部标题</span>
+        </div>
+      </template>
+<slot></slot>
+</el-card> -->
+    <el-card header="卡片头部标题">
+      <slot>默认内容。</slot>
+    </el-card>
+  </div>
+</template>
+
+<style lang="less" scoped>
+.chart-card {}
+</style>
--- src/views/main/analysis/dashboard/dashboard.vue
+++ src/views/main/analysis/dashboard/dashboard.vue
@@ -8,16 +8,44 @@
       </template>
     </el-row>
 
+    <el-row :gutter="10">
+      <el-col :span="7" :offset="0">
+        <chart-card>
+          <BaseEchart />
+        </chart-card>
+      </el-col>
+      <el-col :span="10" :offset="0">
+        <chart-card></chart-card>
+      </el-col>
+      <el-col :span="7" :offset="0">
+        <chart-card></chart-card>
+      </el-col>
+    </el-row>
+
+    <el-row :gutter="10">
+      <el-col :span="12" :offset="0">折线图</el-col>
+      <el-col :span="12" :offset="0"></el-col>
+    </el-row>
+
   </div>
 </template>
 
 <script setup lang="ts" name="dashboard">
 import { ref } from 'vue';
 import topPanel from './cpns/topPanel.vue'
+import chartCard from './cpns/chartCard.vue'
+
+import BaseEchart from '@/components/page-echarts'
+
+
 const randNum = (): number => Math.floor(Math.random() * 100000);
 const numArr = ref<number[]>([randNum(), randNum(), randNum(), randNum()]);
 </script>
 
 <style scoped lang="less">
-.dashboard {}
+.dashboard {
+  .el-row {
+    margin-bottom: 10px;
+  }
+}
 </style>
