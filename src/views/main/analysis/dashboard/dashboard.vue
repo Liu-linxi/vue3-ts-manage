2d68e253c43b8c927e4677ee1464fa35436a47d3
linxi <1595818766@qq.com>
2024-09-29 14:44:15 +0800

    修改组件模拟数据

--- src/views/main/analysis/dashboard/dashboard.vue
+++ src/views/main/analysis/dashboard/dashboard.vue
@@ -1,23 +1,23 @@
 <template>
   <div class="dashboard">
-    <div class="topPanel">
-      <top-panel />
+    <el-row :gutter="10">
+      <template v-for="li in numArr" :key="li">
+        <el-col :span="6" :offset="0">
+          <top-panel v-bind="{ value: li }" />
+        </el-col>
+      </template>
+    </el-row>
 
-    </div>
-    dashboard
   </div>
 </template>
 
 <script setup lang="ts" name="dashboard">
+import { ref } from 'vue';
 import topPanel from './cpns/topPanel.vue'
+const randNum = (): number => Math.floor(Math.random() * 100000);
+const numArr = ref<number[]>([randNum(), randNum(), randNum(), randNum()]);
 </script>
 
 <style scoped lang="less">
-.dashboard {
-  .topPanel {
-    display: grid;
-    grid-template-columns: repeat(4, 1fr);
-    grid-gap: 20px;
-  }
-}
+.dashboard {}
 </style>
