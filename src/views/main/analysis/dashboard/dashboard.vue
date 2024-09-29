be69786ad2694799cf60c3ba0f7455f68787dc7c
linxi <1595818766@qq.com>
2024-09-29 17:40:42 +0800

    完善统计视图

--- src/views/main/analysis/dashboard/dashboard.vue
+++ src/views/main/analysis/dashboard/dashboard.vue
@@ -11,20 +11,24 @@
     <el-row :gutter="10">
       <el-col :span="7" :offset="0">
         <chart-card>
-          <BaseEchart />
+          <pie-echart />
         </chart-card>
       </el-col>
       <el-col :span="10" :offset="0">
-        <chart-card></chart-card>
+        <chart-card><line-echart /></chart-card>
       </el-col>
       <el-col :span="7" :offset="0">
-        <chart-card></chart-card>
+        <chart-card><rose-echart /></chart-card>
       </el-col>
     </el-row>
 
     <el-row :gutter="10">
-      <el-col :span="12" :offset="0">折线图</el-col>
-      <el-col :span="12" :offset="0"></el-col>
+      <el-col :span="12" :offset="0">
+        <chart-card>折线图</chart-card>
+      </el-col>
+      <el-col :span="12" :offset="0">
+        <chart-card></chart-card>
+      </el-col>
     </el-row>
 
   </div>
@@ -34,8 +38,8 @@
 import { ref } from 'vue';
 import topPanel from './cpns/topPanel.vue'
 import chartCard from './cpns/chartCard.vue'
+import { PieEchart, LineEchart, RoseEchart } from '@/components/page-echarts'
 
-import BaseEchart from '@/components/page-echarts'
 
 
 const randNum = (): number => Math.floor(Math.random() * 100000);
